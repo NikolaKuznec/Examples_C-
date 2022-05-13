1dcfce3247892f7c16def65b8ba19eddf568af82
NikolaKuznets <n088v@icloud.com>
2022-05-13 11:03:23 +0300

    Add 003

---
+++ Examples000/Examples003/Program.cs
@@ -0,0 +1,31 @@
+﻿string first = Console.ReadLine();
+
+if (first == "1")
+{
+    Console.WriteLine ("Пн");
+}
+if (first == "2")
+{
+    Console.WriteLine ("Вт");
+}
+if (first == "3")
+{
+    Console.WriteLine ("Ср");
+}
+if (first == "4")
+{
+    Console.WriteLine ("Чт");
+}
+if (first == "5")
+{
+    Console.WriteLine ("Пт");
+}
+if (first == "6")
+{
+    Console.WriteLine ("Субб");
+}
+if (first == "7")
+{
+    Console.WriteLine ("Воск");
+}
+
