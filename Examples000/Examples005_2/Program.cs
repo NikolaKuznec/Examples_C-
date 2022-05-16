95c4db8aefc1c5deb8f412049624db0711632680
NikolaKuznets <n088v@icloud.com>
2022-05-16 16:44:26 +0300

    Add 005_2

---
+++ Examples000/Examples005_2/Program.cs
@@ -0,0 +1,32 @@
+﻿//string s = Console.ReadLine();
+// int LastNumber(int a)
+// {
+//     return a % 10;
+// }
+
+// Console.Write(LastNumber(152));
+
+void Func(int a, int b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+
+    else
+    {
+        Console.WriteLine("Некратно");
+    }
+
+
+}
+
+int a = 34;
+int b = 6;
+Func(a, b);
+
+
+int a = 34;
+int b = 5;
+Func(a, b);
+
