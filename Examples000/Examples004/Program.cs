c545b6f2d86ad1825c61b22dfde3b53642bd6f1c
NikolaKuznets <n088v@icloud.com>
2022-05-16 16:43:24 +0300

    Add 004

---
+++ Examples000/Examples004/Program.cs
@@ -0,0 +1,17 @@
+﻿// string s_a = Console.ReadLine();
+// int a = int.Parse(s_a);
+// int x;
+// x = a * a + a + 2;
+// Console.WriteLine(x);
+//
+// string s = Console.ReadLine();
+// int x = int.Parse(s);
+// int y = -x;
+// while (y <= x)
+// {
+//     Console.Write($"{y} ");
+//     y++;
+// }
+//
+// string s = Console.ReadLine();
+// int x = int.Parse(s);
