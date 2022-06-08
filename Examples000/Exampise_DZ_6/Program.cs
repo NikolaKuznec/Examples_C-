0cbb63168e06adcc4b80d231918aa150b41e0bbe
NikolaKuznec <n088v@icloud.com>
2022-06-08 11:36:24 +0300

    Add DZ_6

---
+++ Examples000/Exampise_DZ_6/Program.cs
@@ -0,0 +1,77 @@
+﻿//Задача №1) Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// int CountNum()
+// {
+//     int count = 0;
+//     string number;
+//     while (true)
+//     {
+//         Console.Write("enter number : ");
+//         number = Console.ReadLine();
+//         if (number == "") return count;
+//         else if (int.Parse(number) > 0) count +=1;
+//     }
+// }
+// Console.WriteLine(CountNum());
+
+
+// Задача № 2) Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// void Summ (double b1, double k1, double b2, double k2)
+// {
+//     double x = (b1 - b2) / (k1 - k2);
+//     double y = k1 * x + b1;
+//     Console.WriteLine($"Intersection point: ({x}; {y}) ");
+// }
+// Summ(2,5,4,9);
+
+// Напишите программу, которая будет создавать копию заданного 2-мерного массива с помощью поэлементного копирования.
+void PrintArray(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+int[,] CopyNum (int[,]arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int[,] new_arr = new int[row_size, column_size];
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            new_arr[i, j] = arr[i, j];
+        }
+    }
+    return new_arr;
+
+}
+int[,] arr_1 = MassNums(2, 5, 1, 11);
+PrintArray(arr_1);
+int[,] arr_1_new = CopyNum(arr_1);
+PrintArray(arr_1_new);
+
+int[,] arr_2 = MassNums(3, 6, 10, 21);
+PrintArray(arr_2);
+int[,] arr_2_new = CopyNum(arr_2);
+PrintArray(arr_2_new); 
--- Examples000/Test/Program.cs
+++ Examples000/Test/Program.cs
@@ -10,7 +10,6 @@
 //         {
 //             Console.WriteLine("IV");
 //         }
-
 //     }
 //     else if (x < 0)
 //     {
@@ -22,9 +21,7 @@
 //         {
 //             Console.WriteLine("III");
 //         }
-
 //     }
-
 // }
 //     Quarters(2, 2);
 //     Quarters(-2, 2);
@@ -71,14 +68,16 @@
 
 // }
 // Console.WriteLine(distance(3, 6, 2, 1));
-// 
+
+
+
+
 // Задача №3 Напишите програаму, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N 
 //
 // void Squares(int num)
 // {
 //     Console.Write($"{num} - >");
 //     int i = 1;
-
 //      while (num >= i)
 //     {
 //         Console.Write(Math.Pow(i, 2));
@@ -99,13 +98,13 @@
 //         all_sum += i;
 //     }
 //     return all_sum;
-
 // }
-
 // Console.WriteLine(SumNum(4));
 // Console.WriteLine(SumNum(8));
 // Console.WriteLine(SumNum(2));
-// 
+
+
+
 // Напишите программу, которая принимает число на вход число и выдает количество цифр в числе.
 // вариант №1
 // void sum(int num)
@@ -116,7 +115,6 @@
 //         num = num / 10;
 //         i = i + 1;
 //     }
-
 //     Console.WriteLine(i);
 // }
 // sum(12345);
@@ -127,11 +125,12 @@
 //     string result = num.ToString();
 //     return result.Length;
 // }
-
 // Console.WriteLine(sum(1234));
 // Console.WriteLine(sum(123));
 // Console.WriteLine(sum(54322));
-// 
+
+
+
 // Напишите пограмму, которая принимает на вход число N и выводаёт произведение чисел от 1 до N
 // 
 // int PowNum(int num)
@@ -141,11 +140,13 @@
 //     {
 //         all_pow *= i;
 //     }
-
 //     return all_pow;
 // }
 // Console.WriteLine(PowNum(5));
-// 
+
+
+
+
 // Напишите программу, которая выводит массив из 8элементов, заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 // 
@@ -171,7 +172,6 @@
 //     int size = arr.Length;
 //     for (int i = 0; i < size - 1; i++)
 //     {
-
 //         int n_max = i;
 //         for (int j = i + 1; j < size; j++)
 //         {
@@ -181,7 +181,6 @@
 //            }  
 //         }
 //       (arr[i], arr[n_max]) = (arr[n_max], arr[i]);
-
 //     }
 // }
 // int[] my_arr = { 78, -32, 5, 39, 59, -7, -78, 3, 76, };
@@ -189,13 +188,16 @@
 // SelectionSort(my_arr);
 // Print(my_arr);
 // 
+
+
+
+
 // 1) Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]
 //    найдите сумму отрицательных элементов массива. 
 // 
 // void Print(int[]arr)
 // {
 //     int size = arr.Length;
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         Console.Write($"{arr[i]} ");
@@ -205,19 +207,16 @@
 // int[] MassNums(int size)
 // {
 //     int[]arr = new int[size];
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         arr[i] = new Random().Next(-9, 10);
 //     }
 //     return arr;
-
 // }
 // void SumMaxMin(int[] arr)
 // {
 //     int s_min, s_max;
 //     s_min = s_max = 0;
-
 //     for (int i = 0; i < arr.Length; i++)
 //     {
 //         if (arr[i] >= 0)
@@ -228,27 +227,24 @@
 //         {
 //             s_min += arr[i];
 //         }      
-
 //     }
 //     Console.WriteLine($"sum posit{s_max}, sum neg{s_min}");
-
 // }
-
 // int[] arr_1 = MassNums(12);
 // Print(arr_1);
 // SumMaxMin(arr_1);
 // int[] arr_2 = MassNums(12);
 // Print(arr_2);
 // SumMaxMin(arr_2);
-// 
+
+
+
 // Напишите программу замена элементов массива:
 // положительны элементы заменить на соответствующие отрицательные, и на оборот. 
 // 
-
 // void Print(int[] arr)
 // {
 //     int size = arr.Length;
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         Console.Write($"{arr[i]} ");
@@ -258,13 +254,11 @@
 // int[] MassNums(int size)
 // {
 //     int[] arr = new int[size];
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         arr[i] = new Random().Next(-5, 5);
 //     }
 //     return arr;
-
 // }
 // void ChangeNum(int[] arr)
 // {
@@ -273,7 +267,6 @@
 //         arr[i] = arr[i] * -1;
 //     }
 // }
-
 // int[] arr_1 = MassNums(10);
 // Print(arr_1);
 // ChangeNum(arr_1);
@@ -281,14 +274,14 @@
 // Print(arr_2);
 // ChangeNum(arr_2);
 
+
+
 // Задайте массив. Напишите программу, которая определяет, присутствие ли заданное число в массиве.
 // 4 [6,7,19,345,3] - > нет 
 // 3 [7,7,19,345,3] - > да 
-
 // void Print(int[] arr)
 // {
 //     int size = arr.Length;
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         Console.Write($"{arr[i]} ");
@@ -298,7 +291,6 @@
 // int[] MassNums(int size)
 // {
 //     int[] arr = new int[size];
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         arr[i] = new Random().Next(0, 10);
@@ -317,21 +309,20 @@
 //     }
 //     Console.WriteLine(flag);
 // }
-
 // int[] arr_1 = MassNums(10);
 // Print(arr_1);
 // Search(5, arr_1);
-
 // int[] arr_2 = MassNums(10);
 // Print(arr_2);
 // Search(5, arr_2);
+
+
 
 // Задайте одномерный массив из 123 случайных чисел.
 // Найдите количество элементов массива, значение которых лежат в отрезке [10,99] 
 // void PrintArray(int[] arr)
 // {
 //     int size = arr.Length;
-
 //     for (int i = 0; i < size; i++)
 //     {
 //         Console.Write($"{arr[i]} ");
@@ -349,7 +340,6 @@
 //     return arr;
 // }
 // int[] arr = MassNums(20);
-
 // int count = 0;
 // for (int i = 0; i < arr.Length; i++)
 // {
@@ -359,3 +349,114 @@
 // PrintArray(arr);
 
 
+
+// Урок №6 Двухмерные массивы и рекурсия. 
+// Задача №1 напишите программу, которая перевернёт одномерный массив.
+//  (последний элемент будет на первом месте, а первый - на поледнем и ТД. )
+// void PrintArray(int[] arr)
+// {
+//     int size = arr.Length;
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write($"{arr[i]} ");
+//     }
+//     Console.WriteLine(arr[size - 1]);
+// }
+// int[] MassNums(int size, int from, int to)
+// {
+//     int[] arr = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = new Random().Next(from, to);
+//     }
+//     return arr;
+// }
+// int[] RevMas(int[] arr)
+// {
+//     int size = arr.Length - 1;
+//     for (int i = 0; i < size / 2 + 1; i++)
+//     {
+//         (arr[i], arr[size - i]) = (arr[size - i], arr[i]);
+//     }
+//     return arr;
+// }
+// int[] arr_1 = MassNums(8, 5, 21);
+// PrintArray(arr_1);
+// RevMas(arr_1);
+// PrintArray(arr_1);
+
+
+
+
+// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник 
+// со сторонами такой длинны. 
+// string check(int a, int b, int c)
+// {
+//     if (a < b + c && b < a + c && c < a + b)
+//     return "no";
+//     return "yes";
+// }
+// Console.WriteLine (check(4,6,7));
+
+
+
+//  Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// void binary(int num)
+// {
+//     string i = "";
+//     while (num > 0)
+//     {
+//         i = num % 2 + i;
+//         num = num / 2;
+//     }
+//     Console.Write(i);
+// }
+// binary(2);
+
+
+// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//  Первые два числа Фибоначи: 0 и 1.
+// void Fibonacci(int num)
+// {
+//     int a = 0;
+//     int b = 1;
+//     for (int i = 0; i < num; i++)
+//     {      
+//         Console.Write($"{a} ");
+//         (a, b) = (b, a + b);
+//     }
+// }
+// Fibonacci(7);
+
+
+// Напишите программу, которая будет создавать копию заданного массива 
+//  с помощью поэлементного копирования. 
+void PrintArray(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine(arr[size - 1]);
+}
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+int [] CopyMass (int []arr)
+{
+    int size = arr.Length;
+    int []new_arr = new int [size];
+    for (int i= 0; i < size; i++)
+    {
+        new_arr[i] = arr[i];
+    }
+    return new_arr;
+}
+int arr_1 = MassNums
